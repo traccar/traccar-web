b08779974ece147474eb53b8e0f75640e0810069
Anton Tananaev <anton.tananaev@gmail.com>
2013-01-11 22:35:33 +1300

    Fix war package script

--- package.sh
+++ package.sh
@@ -1,11 +1,9 @@
 rm -f traccar-web.war
 mkdir tmp/
-mv war/WEB-INF/classes/org/traccar/web/client/ tmp/
 mv war/WEB-INF/deploy/ tmp/
 mv war/WEB-INF/lib/mysql-* tmp/
 jar cv -C war/ . > traccar-web.war
 mv tmp/mysql-* war/WEB-INF/lib/
 mv tmp/deploy/ war/WEB-INF/
-mv tmp/client/ war/WEB-INF/classes/org/traccar/web/
 rm -r -f tmp/
 
