bb396f57083ef93ac64c69054ff2397052a1a209
Abyss777 <abyss@fox5.ru>
2016-11-28 15:02:41 +0500

    Use String

--- web/app/view/Login.js
+++ web/app/view/Login.js
@@ -43,7 +43,7 @@ Ext.define('Traccar.view.Login', {
         items: [{
             xtype: 'image',
             src: 'logo.svg',
-            alt: 'Traccar',
+            alt: Strings.loginLogo,
             width: 180,
             height: 48,
             style: {
