1aae9dee5ecd797c3c2d2a6e985f605d783620ee
Abyss777 <abyss@fox5.ru>
2016-12-01 18:05:10 +0500

    Improve firing event

--- web/app/controller/Root.js
+++ web/app/controller/Root.js
@@ -96,7 +96,7 @@ Ext.define('Traccar.controller.Root', {
         }
         eventId = Ext.Object.fromQueryString(window.location.search).eventId;
         if (eventId) {
-            this.application.fireEvent('showSingleEvent', eventId);
+            this.fireEvent('showsingleevent', eventId);
             //this.removeUrlParameter('eventId');
         }
     },
