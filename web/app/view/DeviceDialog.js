7bdbea99ae60ee6dab11a892b2336a4dfa45da5e
Abyss777 <abyss@fox5.ru>
2017-03-10 17:37:42 +0500

    Group device fields

--- web/app/view/DeviceDialog.js
+++ web/app/view/DeviceDialog.js
@@ -23,52 +23,62 @@ Ext.define('Traccar.view.DeviceDialog', {
     items: {
         xtype: 'form',
         items: [{
-            xtype: 'textfield',
-            name: 'name',
-            fieldLabel: Strings.sharedName,
-            allowBlank: false
+            xtype: 'fieldset',
+            title: Strings.sharedRequired,
+            items: [{
+                xtype: 'textfield',
+                name: 'name',
+                fieldLabel: Strings.sharedName,
+                allowBlank: false
+            }, {
+                xtype: 'textfield',
+                name: 'uniqueId',
+                fieldLabel: Strings.deviceIdentifier,
+                allowBlank: false
+            }]
         }, {
-            xtype: 'textfield',
-            name: 'uniqueId',
-            fieldLabel: Strings.deviceIdentifier,
-            allowBlank: false
-        }, {
-            xtype: 'combobox',
-            name: 'groupId',
-            fieldLabel: Strings.groupParent,
-            store: 'Groups',
-            queryMode: 'local',
-            displayField: 'name',
-            valueField: 'id'
-        }, {
-            xtype: 'textfield',
-            name: 'phone',
-            fieldLabel: Strings.sharedPhone
-        }, {
-            xtype: 'textfield',
-            name: 'model',
-            fieldLabel: Strings.deviceModel
-        }, {
-            xtype: 'textfield',
-            name: 'contact',
-            fieldLabel: Strings.deviceContact
-        }, {
-            xtype: 'combobox',
-            name: 'category',
-            fieldLabel: Strings.deviceCategory,
-            store: 'DeviceImages',
-            queryMode: 'local',
-            displayField: 'name',
-            valueField: 'key',
-            editable: false,
-            listConfig: {
-                getInnerTpl: function () {
-                    return '<table><tr valign="middle" ><td><div align="center" style="width:40px;height:40px;" >' +
-                    '{[new XMLSerializer().serializeToString(Traccar.DeviceImages.getImageSvg(' +
-                    'Traccar.Style.mapColorOnline, false, 0, values.key))]}</div></td>' +
-                    '<td>- {name}</td></tr></table>';
+            xtype: 'fieldset',
+            title: Strings.sharedPreferences,
+            collapsible: true,
+            collapsed: true,
+            items: [{
+                xtype: 'combobox',
+                name: 'groupId',
+                fieldLabel: Strings.groupParent,
+                store: 'Groups',
+                queryMode: 'local',
+                displayField: 'name',
+                valueField: 'id'
+            }, {
+                xtype: 'textfield',
+                name: 'phone',
+                fieldLabel: Strings.sharedPhone
+            }, {
+                xtype: 'textfield',
+                name: 'model',
+                fieldLabel: Strings.deviceModel
+            }, {
+                xtype: 'textfield',
+                name: 'contact',
+                fieldLabel: Strings.deviceContact
+            }, {
+                xtype: 'combobox',
+                name: 'category',
+                fieldLabel: Strings.deviceCategory,
+                store: 'DeviceImages',
+                queryMode: 'local',
+                displayField: 'name',
+                valueField: 'key',
+                editable: false,
+                listConfig: {
+                    getInnerTpl: function () {
+                        return '<table><tr valign="middle" ><td><div align="center" style="width:40px;height:40px;" >' +
+                        '{[new XMLSerializer().serializeToString(Traccar.DeviceImages.getImageSvg(' +
+                        'Traccar.Style.mapColorOnline, false, 0, values.key))]}</div></td>' +
+                        '<td>- {name}</td></tr></table>';
+                    }
                 }
-            }
+            }]
         }]
     }
 });
