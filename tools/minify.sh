be1f9e7414b2f5d54035868fe98e6408ab931135
jon-stumpf <jon.stumpf@gmail.com>
2017-09-29 19:23:04 -0400

    Rewrote tools/minify.sh;

--- tools/minify.sh
+++ tools/minify.sh
@@ -1,14 +1,62 @@
 #!/bin/sh
 
-cd $(dirname $0)/../web
+# Sencha Variables
+if [ -z "${SENCHAROOT}" ]
+then
+	SENCHAROOT="../../.."
+fi
 
-SDK="../../../ext-6.2.0"
+SDK="${SENCHAROOT}/ext-6.2.0"
+CORE="${SDK}/packages/core"
+CLASSIC="${SDK}/classic/classic"
+CMDDIR="${SENCHAROOT}/Cmd"
 
-sencha compile --classpath=app.js,app,$SDK/packages/core/src,$SDK/packages/core/overrides,$SDK/classic/classic/src,$SDK/classic/classic/overrides \
-       exclude -all \
-       and \
-       include -recursive -file app.js \
-       and \
-       exclude -namespace=Ext \
-       and \
-       concatenate -closure app.min.js
+if [ ${#} -gt 1 ]
+then
+	echo "usage: $(basename ${0}) [ <traccar-web directory> ]"
+	exit 1
+fi
+
+if [ ${#} -eq 0 ]
+then
+	# Maintain default behavior where CWD is assumed to be traccar-web/tools.
+	TRACCARWEB="$(dirname $0)/../web"
+else
+	TRACCARWEB="${1}"
+fi
+
+if [ ! -d "${TRACCARWEB}" ]
+then
+	echo "${TRACCARWEB}: directory not found" 1>&2
+	exit 2
+fi
+
+INPUTFILE="${TRACCARWEB}/app.js"
+OUTPUTFILE="${TRACCARWEB}/app.min.js"
+APPDIR="${TRACCARWEB}/app"
+
+# Assemble CLASSPATH
+CLASSPATH="${INPUTFILE},${APPDIR}"
+
+for d in "${CORE}" "${CLASSIC}"
+do
+	for e in "src" "overrides"
+	do
+		if [ ! -d "${d}/${e}" ]
+		then
+			echo "${d}/${e}: directory not found" 1>&2
+			exit 2
+		fi
+
+		CLASSPATH="${CLASSPATH},${d}/${e}"
+	done
+done
+
+# Minify
+"${CMDDIR}/sencha" compile \
+	--classpath="${CLASSPATH}" \
+	exclude -all and \
+	include -recursive -file "${INPUTFILE}" and \
+	exclude -namespace=Ext and \
+	concatenate -closure "${OUTPUTFILE}"
+
