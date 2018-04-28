1dd02a6d4d0c58d660bc40b5b70173bb2e66a4dc
Mateusz Jończyk <mat.jonczyk@o2.pl>
2018-04-28 19:43:21 +0200

    minify.sh: use environment variable EXTJS_PATH
    Allow setting location of ext-6.2.0 directory by an environment
    variable. People can have ext-6.2.0 in various locations.

--- tools/minify.sh
+++ tools/minify.sh
@@ -2,7 +2,8 @@
 
 cd $(dirname $0)/../web
 
-EXT="../../../ext-6.2.0"
+# Use the value of $EXTJS_PATH if it is defined, '../../../ext-6.2.0' otherwise
+EXT=${EXTJS_PATH:-'../../../ext-6.2.0'}
 
 sencha compile --classpath=app.js,app,$EXT/packages/core/src,$EXT/packages/core/overrides,$EXT/classic/classic/src,$EXT/classic/classic/overrides \
        exclude -all \
