4675139fc0dcf2f9f0acc3171af04fba86de0f79
Anton Tananaev <anton.tananaev@gmail.com>
2018-09-07 17:18:37 +1200

    Implement native notifications

--- modern/src/SocketContoller.js
+++ modern/src/SocketContoller.js
@@ -2,6 +2,23 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 import { updatePositions } from './actions';
 
+const displayNotifications = events => {
+  if ("Notification" in window) {
+    if (Notification.permission === "granted") {
+      for (const event of events) {
+        const notification = new Notification(`Event: ${event.type}`);
+        setTimeout(notification.close.bind(notification), 4 * 1000);
+      }
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission(permission => {
+        if (permission === "granted") {
+          displayNotifications(events);
+        }
+      });
+    }
+  }
+};
+
 class SocketController extends Component {
   connectSocket() {
     const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
@@ -15,6 +32,9 @@ class SocketController extends Component {
       const data = JSON.parse(event.data);
       if (data.positions) {
         this.props.dispatch(updatePositions(data.positions));
+      }
+      if (data.events) {
+        displayNotifications(data.events);
       }
     }
   }
