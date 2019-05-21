728f3c1b0beeccb6b3e376c57f3f2f61d2145f40
Tom Groshong <tom@groshong.software>
2019-05-21 16:14:39 -0400

    Fix proxy setup with customizable file
    The previous proxy wasn't specific enough and occasionally caused issues. This
    change uses the provided escape hatch for customizing the proxy used by
    `react-scripts`.

---
+++ modern/src/setupProxy.js
@@ -0,0 +1,8 @@
+const proxy = require("http-proxy-middleware");
+
+/**
+ * Fully customizable proxy used by react-scripts
+ */
+module.exports = function(app) {
+  app.use(proxy("/api", { target: "http://localhost:8082/" }));
+};
