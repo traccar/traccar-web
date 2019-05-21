d3aa05da229bba9238cbf4b8dd857ba63a81a112
Tom Groshong <tom@groshong.software>
2019-05-21 16:28:56 -0400

    Fix bug in proxy.  Enable websocket proxying.

--- modern/src/setupProxy.js
+++ modern/src/setupProxy.js
@@ -4,5 +4,5 @@ const proxy = require("http-proxy-middleware");
  * Fully customizable proxy used by react-scripts
  */
 module.exports = function(app) {
-  app.use(proxy("/api", { target: "http://localhost:8082/" }));
+  app.use(proxy("/api", { target: "http://localhost:8082/", ws: true }));
 };
