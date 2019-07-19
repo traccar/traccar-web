67f7f30d2e646f2caf7bdc9b8a4af6522ee90693
edvalley <52469633+edvalley@users.noreply.github.com>
2019-07-18 21:49:15 -0400

    Fix misleading displayed label (#751)

--- web/app/view/dialog/Server.js
+++ web/app/view/dialog/Server.js
@@ -47,7 +47,7 @@ Ext.define('Traccar.view.dialog.Server', {
                 xtype: 'unescapedTextField',
                 reference: 'mapUrlField',
                 name: 'mapUrl',
-                fieldLabel: Strings.mapCustom
+                fieldLabel: Strings.mapCustomLabel
             }, {
                 xtype: 'numberfield',
                 reference: 'latitude',
