13fb1b0ec962b3262acd4d71ea6b382ce23cafaa
Anton Tananaev <anton.tananaev@gmail.com>
2020-03-18 21:44:34 -0700

    Fix list items

--- modern/src/DeviceList.js
+++ modern/src/DeviceList.js
@@ -3,13 +3,13 @@ import { connect } from 'react-redux';
 import { updateDevices } from './actions';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Divider from '@material-ui/core/Divider';
 
 const mapStateToProps = state => ({
   devices: state.devices
@@ -30,9 +30,11 @@ class DeviceList extends Component {
     const devices = this.props.devices.map(device =>
       <Fragment key={device.id.toString()}>
         <ListItem button>
-          <Avatar>
-            <LocationOnIcon />
-          </Avatar>
+          <ListItemAvatar>
+            <Avatar>
+              <LocationOnIcon />
+            </Avatar>
+          </ListItemAvatar>
           <ListItemText primary={device.name} secondary={device.uniqueId} />
           <ListItemSecondaryAction>
             <IconButton>
@@ -40,9 +42,6 @@ class DeviceList extends Component {
             </IconButton>
           </ListItemSecondaryAction>
         </ListItem>
-        <li>
-          <Divider inset />
-        </li>
       </Fragment>
     );
 
