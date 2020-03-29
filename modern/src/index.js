800da2aef77cd0e64d39b419929f7ac97db47f21
Anton Tananaev <anton.tananaev@gmail.com>
2020-03-29 14:49:19 -0700

    Updated service worker

--- modern/src/index.js
+++ modern/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 ReactDOM.render((
@@ -16,4 +16,4 @@ ReactDOM.render((
   </Provider>
 ), document.getElementById('root'));
 
-registerServiceWorker();
+serviceWorker.register();
