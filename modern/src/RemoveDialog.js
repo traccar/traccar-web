66cdc1e5bff5331bd4d23038fa032efb39817a1f
Anton Tananaev <anton.tananaev@gmail.com>
2020-06-06 16:02:47 -0700

    Fix import issue

--- modern/src/RemoveDialog.js
+++ modern/src/RemoveDialog.js
@@ -1,4 +1,5 @@
 import t from './common/localization'
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
