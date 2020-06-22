58f79c306a80c5aad32d642b84098a239d95acb4
Anton Tananaev <anton.tananaev@gmail.com>
2020-06-21 22:49:42 -0700

    Support static website

--- modern/src/index.js
+++ modern/src/index.js
@@ -1,7 +1,7 @@
 import 'typeface-roboto'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -10,9 +10,9 @@ import store from './store';
 
 ReactDOM.render((
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 ), document.getElementById('root'));
 
