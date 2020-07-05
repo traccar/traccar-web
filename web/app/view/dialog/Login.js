8e3d7b88d4c05fa6c5a75907b850b897a0da9991
Anton Tananaev <anton.tananaev@gmail.com>
2020-07-04 23:18:37 -0700

    Add link for modern app

--- web/app/view/dialog/Login.js
+++ web/app/view/dialog/Login.js
@@ -28,6 +28,11 @@ Ext.define('Traccar.view.dialog.Login', {
     header: false,
     closable: false,
 
+    listeners: {
+        afterrender: 'handleAdded',
+        beforedestroy: 'handleRemoved'
+    },
+
     items: {
         xtype: 'form',
         reference: 'form',
