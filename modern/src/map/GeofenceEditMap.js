eecb7e6ef829cb8f7187975ac25d4f4623f34cc9
Aliber009 <57564469+Aliber009@users.noreply.github.com>
2021-04-13 11:43:26 +0000

    Update GeofenceEditMap.js
    I converted the class component in my wished last edit, to a function component, so the edits are easy to see now
    I hope it works

--- modern/src/map/GeofenceEditMap.js
+++ modern/src/map/GeofenceEditMap.js
@@ -1,21 +1,103 @@
-import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+import * as React from 'react';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
-import { useEffect } from 'react';
-
+import theme from '@mapbox/mapbox-gl-draw/src/lib/theme';
 import { map } from './Map';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
+import {useState,useEffect} from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+import {
+  CircleMode,
+  DragCircleMode,
+  DirectMode,
+  SimpleSelectMode
+} from 'mapbox-gl-draw-circle';
+
+class extendDrawBar {
+  constructor(opt) {
+    let ctrl = this;
+    ctrl.draw = opt.draw;
+    ctrl.buttons = opt.buttons || [];
+    ctrl.onAddOrig = opt.draw.onAdd;
+    ctrl.onRemoveOrig = opt.draw.onRemove;
+  }
+  onAdd(map) {
+    let ctrl = this;
+    ctrl.map = map;
+    ctrl.elContainer = ctrl.onAddOrig(map);
+    ctrl.buttons.forEach((b) => {
+      ctrl.addButton(b);
+    });
+    return ctrl.elContainer;
+  }
+  onRemove(map) {
+    let ctrl = this;
+    ctrl.buttons.forEach((b) => {
+      ctrl.removeButton(b);
+    });
+    ctrl.onRemoveOrig(map);
+  }
+  addButton(opt) {
+    let ctrl = this;
+    var elButton = document.createElement('button');
+    elButton.className = 'fa fa-circle-o';
+    if (opt.classes instanceof Array) {
+      opt.classes.forEach((c) => {
+        elButton.classList.add(c);
+      });
+    }
+    elButton.addEventListener(opt.on, opt.action);
+    ctrl.elContainer.appendChild(elButton);
+    opt.elButton = elButton;
+  }
+  removeButton(opt) {
+    opt.elButton.removeEventListener(opt.on, opt.action);
+    opt.elButton.remove();
+  }
+}
 
 const draw = new MapboxDraw({
-  displayControlsDefault: false,
+ displayControlsDefault: false,
   controls: {
+    point: true,
+    line_string: true,
     polygon: true,
     trash: true,
+  
   },
+ 
+  styles: theme,
+  
+ 
+  modes: {
+   ...MapboxDraw.modes,
+    draw_circle  : CircleMode,
+    drag_circle  : DragCircleMode,
+    direct_select: DirectMode,
+    simple_select: SimpleSelectMode
+  },
+    
+  
+  defaultMode: "simple_select",
+  userProperties: true
 });
-
+var drawBar = new extendDrawBar({
+  draw: draw,
+  
+  buttons: [
+    {
+      on: 'click',
+      action: function circle(){
+      draw.changeMode('draw_circle');
+      },
+      classes: []
+    }
+  ] 
+}); 
 const GeofenceEditMap = () => {
   useEffect(() => {
-    map.addControl(draw, 'top-left');
-    return () => map.removeControl(draw);
+    map.addControl(drawBar, 'top-left');
+    return () => map.removeControl(drawBar);
   }, []);
 
   return null;
