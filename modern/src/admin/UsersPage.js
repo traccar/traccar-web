2cffd8e6ce2208d233b11deffa1cceb98ec934ab
Aliber009 <57564469+Aliber009@users.noreply.github.com>
2021-04-16 21:51:19 +0000

    add admin permissions to userPage
    added a checkbox to change user to admin or admin to user permission in the userPage

--- modern/src/admin/UsersPage.js
+++ modern/src/admin/UsersPage.js
@@ -1,70 +1,115 @@
 import React, { useState } from 'react';
-import MainToolbar from '../MainToolbar';
-import { TableContainer, Table, TableRow, TableCell, TableHead, TableBody, makeStyles, IconButton } from '@material-ui/core';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import t from '../common/localization';
-import { useEffectAsync } from '../reactHelper';
-import EditCollectionView from '../EditCollectionView';
-import { formatBoolean } from '../common/formatter';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import t from './common/localization';
+import userAttributes from './attributes/userAttributes';
+import EditItemView from './EditItemView';
+import { Accordion, AccordionSummary, AccordionDetails, makeStyles, Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import EditAttributesView from './attributes/EditAttributesView';
+import LinkField from './form/LinkField';
 
-const useStyles = makeStyles(theme => ({
-  columnAction: {
-    width: theme.spacing(1),
-    padding: theme.spacing(0, 1),
+const useStyles = makeStyles(() => ({
+  details: {
+    flexDirection: 'column',
   },
 }));
 
-const UsersView = ({ updateTimestamp, onMenuClick }) => {
+const UserPage = () => {
   const classes = useStyles();
 
-  const [items, setItems] = useState([]);
-
-  useEffectAsync(async () => {
-    const response = await fetch('/api/users');
-    if (response.ok) {
-      setItems(await response.json());
-    }
-  }, [updateTimestamp]);
+  const [item, setItem] = useState();
+  const [checked, setChecked] = useState(false);
 
   return (
-    <TableContainer>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell className={classes.columnAction} />
-          <TableCell>{t('sharedName')}</TableCell>
-          <TableCell>{t('userEmail')}</TableCell>
-          <TableCell>{t('userAdmin')}</TableCell>
-          <TableCell>{t('sharedDisabled')}</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {items.map((item) => (
-          <TableRow key={item.id}>
-            <TableCell className={classes.columnAction} padding="none">
-              <IconButton onClick={(event) => onMenuClick(event.currentTarget, item.id)}>
-                <MoreVertIcon />
-              </IconButton>
-            </TableCell>
-            <TableCell>{item.name}</TableCell>
-            <TableCell>{item.email}</TableCell>
-            <TableCell>{formatBoolean(item, 'administrator')}</TableCell>
-            <TableCell>{formatBoolean(item, 'disabled')}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-    </TableContainer>
+    <EditItemView endpoint="users" item={item} setItem={setItem}>
+      {item &&
+        <>
+          <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="subtitle1">
+                {t('sharedRequired')}
+              </Typography>
+            </AccordionSummary>
+            {console.log(item)}
+            <AccordionDetails className={classes.details}>
+              <TextField
+                margin="normal"
+                value={item.name || ''}
+                onChange={event => setItem({...item, name: event.target.value})}
+                label={t('sharedName')}
+                variant="filled" />
+              <TextField
+                margin="normal"
+                value={item.email || ''}
+                onChange={event => setItem({...item, email: event.target.value})}
+                label={t('userEmail')}
+                variant="filled" />
+              <TextField
+                margin="normal"
+                type="password"
+                onChange={event => setItem({...item, password: event.target.value})}
+                label={t('userPassword')}
+                variant="filled" />
+                 
+            </AccordionDetails>
+            
+            <Typography style={{display:"flex", marginLeft:25,marginBottom:-35}} variant="button"  >ADMIN </Typography>
+            <Checkbox style={{marginLeft:300}}
+                checked={item.administrator || false}
+                onChange={e=>setItem({...item,administrator: (e.target.checked)})}
+                color="primary"
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+                />
+          </Accordion>
+          <Accordion>
+          
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="subtitle1">
+                {t('sharedAttributes')}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails className={classes.details}>
+              <EditAttributesView
+                attributes={item.attributes}
+                setAttributes={attributes => setItem({...item, attributes})}
+                definitions={userAttributes}
+                />
+            </AccordionDetails>
+          </Accordion>
+          {item.id &&
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant="subtitle1">
+                  {t('sharedConnections')}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails className={classes.details}>
+                <LinkField
+                  margin="normal"
+                  endpointAll="/api/devices?all=true"
+                  endpointLinked={"/api/devices?userId=" + item.id}
+                  baseId={item.id}
+                  keyBase="userId"
+                  keyLink="deviceId"
+                  label={t('deviceTitle')}
+                  variant="filled" />
+                <LinkField
+                  margin="normal"
+                  endpointAll="/api/groups?all=true"
+                  endpointLinked={"/api/groups?userId=" + item.id}
+                  baseId={item.id}
+                  keyBase="userId"
+                  keyLink="groupId"
+                  label={t('settingsGroups')}
+                  variant="filled" />
+              </AccordionDetails>
+            </Accordion>
+          }
+        </>
+      }
+    </EditItemView>
   );
 }
 
-const UsersPage = () => {
-  return (
-    <>
-      <MainToolbar />
-      <EditCollectionView content={UsersView} editPath="/user" endpoint="users" />
-    </>
-  );
-}
-
-export default UsersPage;
+export default UserPage;
