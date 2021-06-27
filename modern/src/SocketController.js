02af1dc85084d46bd57bb2cc89d8b5aa9103ac9f
Anton Tananaev <anton.tananaev@gmail.com>
2021-06-26 17:13:34 -0700

    Remove logging

--- modern/src/SocketController.js
+++ modern/src/SocketController.js
@@ -27,12 +27,10 @@ const SocketController = () => {
   const authenticated = useSelector(state => !!state.session.user);
 
   const connectSocket = () => {
-    console.log('connect socket method invoked');
     const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
     const socket = new WebSocket(protocol + '//' + window.location.host + '/api/socket');
 
     socket.onclose = () => {
-      console.log('socket closed');
       setTimeout(() => {console.log('socket reconnection try');connectSocket()}, 60 * 1000);
     };
 
