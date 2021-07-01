36d05b64fbcd52760e023af227748b2efd74d21f
Desmond Kyeremeh <elDekyfin@gmail.com>
2021-07-01 13:53:44 +0000

    Abstract ReportSidebar into SideNav component

---
+++ modern/src/components/SideNav.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Divider,
+  ListSubheader
+} from '@material-ui/core';
+import { Link, useLocation } from 'react-router-dom';
+
+const SideNav = ({ routes }) => {
+  const location = useLocation();
+
+  return (
+    <List disablePadding style={{ paddingTop: '16px' }}>
+      {routes.map((route, index) =>
+        route.subheader ? (
+          <>
+            <Divider />
+            <ListSubheader>{route.subheader}</ListSubheader>
+          </>
+        ) : (
+          <ListItem
+            disableRipple
+            component={Link}
+            key={route.href || route.subheader}
+            button
+            to={route.href}
+            selected={route.href === location.pathname}
+          >
+            <ListItemIcon>{route.icon}</ListItemIcon>
+            <ListItemText primary={route.name} />
+          </ListItem>
+        )
+      )}
+    </List>
+  );
+};
+
+export default SideNav;
