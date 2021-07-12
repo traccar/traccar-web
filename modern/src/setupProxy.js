ae822a2a39a38e3f3715c4573835b0ecd8646fa4
Anton Tananaev <anton.tananaev@gmail.com>
2021-07-11 21:13:04 -0700

    Fix proxy

--- modern/src/setupProxy.js
+++ modern/src/setupProxy.js
@@ -2,7 +2,7 @@
 
 const proxy = require('http-proxy-middleware');
 
-export default (app) => {
+module.exports = (app) => {
   app.use(proxy('/api/socket', { target: `ws://${process.env.REACT_APP_URL_NAME}`, ws: true }));
   app.use(proxy('/api', { target: `http://${process.env.REACT_APP_URL_NAME}` }));
 };
