e66bb788f569fc3123e22372e871759d4d420c52
Desmond Kyeremeh <elDekyfin@gmail.com>
2021-07-23 13:51:48 +0000

    moved stuff to selectors

--- modern/src/common/selectors.js
+++ modern/src/common/selectors.js
@@ -1,3 +1,7 @@
 export const getIsAdmin = (state) => state.session.user?.administrator;
 
 export const getUserId = (state) => state.session.user?.id;
+
+export const getDevices = (state) => Object.values(state.devices.items);
+
+export const getItemPosition = (itemId) => (state) => state.positions.items[itemId];
