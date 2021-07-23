502b4f55f1371e5c47813f5f19908dc32965516b
hieund612 <75609123+hieund612@users.noreply.github.com>
2021-07-23 14:48:14 +0700

    [Modern] Device status colors for the icons in the Devices List

--- modern/src/DevicesList.js
+++ modern/src/DevicesList.js
@@ -76,6 +76,18 @@ const getBatteryStatus = (batteryLevel) => {
   return 'red';
 };
 
+const getStatusColorIcon = (item) => {
+  switch (item.status) {
+    case 'online':
+      return 'green';
+    case 'offline':
+      return 'red';
+    case 'unknown':
+    default:
+      return 'gray';
+  }
+}
+
 const DeviceRow = ({ data, index, style }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -90,7 +102,7 @@ const DeviceRow = ({ data, index, style }) => {
       <Fragment key={index}>
         <ListItem button key={item.id} className={classes.listItem} onClick={() => dispatch(devicesActions.select(item))}>
           <ListItemAvatar>
-            <Avatar>
+            <Avatar style={{ background: getStatusColorIcon(item) }}>
               <img className={classes.icon} src={`images/icon/${item.category || 'default'}.svg`} alt="" />
             </Avatar>
           </ListItemAvatar>
