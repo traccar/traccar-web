a2949a0454b86a70b885d3f559b71959cce975c5
andywarren1969 <56930748+andywarren1969@users.noreply.github.com>
2021-08-26 12:23:43 +0100

    Update BaseMap.js
    Added UK OS maps projection

--- web/app/view/map/BaseMap.js
+++ web/app/view/map/BaseMap.js
@@ -263,6 +263,7 @@ Ext.define('Traccar.view.map.BaseMap', {
 }, function () {
     var projection;
     proj4.defs('EPSG:3395', '+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs');
+	proj4.defs('EPSG:27700', '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs');	
     ol.proj.proj4.register(proj4);
     projection = ol.proj.get('EPSG:3395');
     if (projection) {
