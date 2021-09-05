1aefaf62068c9d74df8cebe39616dc9f8bdc6596
Anton Tananaev <anton.tananaev@gmail.com>
2021-09-05 11:29:52 -0700

    Remove incorrect status

--- modern/src/map/SelectedDeviceMap.js
+++ modern/src/map/SelectedDeviceMap.js
@@ -1,20 +1,9 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
 
-import { ThemeProvider } from '@material-ui/core/styles';
-import maplibregl from 'maplibre-gl';
-import { Provider, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { map } from './Map';
-import store from '../store';
-import StatusView from './StatusView';
-import theme from '../theme';
-
-let popup;
 
 const SelectedDeviceMap = () => {
-  const history = useHistory();
-
   const mapCenter = useSelector((state) => {
     if (state.devices.selectedId) {
       const position = state.positions.items[state.devices.selectedId] || null;
@@ -24,34 +13,6 @@ const SelectedDeviceMap = () => {
     }
     return null;
   });
-
-  const showStatus = (deviceId, coordinates) => {
-    const placeholder = document.createElement('div');
-    ReactDOM.render(
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <StatusView
-            deviceId={deviceId}
-            onShowDetails={(positionId) => history.push(`/position/${positionId}`)}
-            onShowHistory={() => history.push('/replay')}
-            onEditClick={(deviceId) => history.push(`/device/${deviceId}`)}
-          />
-        </ThemeProvider>
-      </Provider>,
-      placeholder,
-    );
-
-    if (popup) {
-      popup.remove();
-    }
-    popup = new maplibregl.Popup({
-      offset: 25,
-      anchor: 'top',
-      closeOnClick: true,
-    });
-
-    popup.setDOMContent(placeholder).setLngLat(coordinates).addTo(map);
-  };
 
   useEffect(() => {
     if (mapCenter) {
