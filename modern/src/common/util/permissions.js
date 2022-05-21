ab1fa14629c205f42a9a65817f2376b8a4f6636b
Anton Tananaev <anton@traccar.org>
2022-05-20 19:23:13 -0700

    Fix indentation

--- modern/src/common/util/permissions.js
+++ modern/src/common/util/permissions.js
@@ -1,35 +1,35 @@
 import { useSelector } from 'react-redux';
 
 export const useAdministrator = () => useSelector((state) => {
-    const admin = state.session.user?.administrator;
-    return admin;
+  const admin = state.session.user?.administrator;
+  return admin;
 });
 
 export const useManager = () => useSelector((state) => {
-    const admin = state.session.user?.administrator;
-    const manager = (state.session.user?.userLimit || 0) > 0
-    return admin || manager;
+  const admin = state.session.user?.administrator;
+  const manager = (state.session.user?.userLimit || 0) > 0;
+  return admin || manager;
 });
 
 export const useReadonly = () => useSelector((state) => {
-    const admin = state.session.user?.administrator;
-    const serverReadonly = state.session.server?.readonly;
-    const userReadonly = state.session.user?.readonly;
-    return !admin && (serverReadonly || userReadonly);
+  const admin = state.session.user?.administrator;
+  const serverReadonly = state.session.server?.readonly;
+  const userReadonly = state.session.user?.readonly;
+  return !admin && (serverReadonly || userReadonly);
 });
 
 export const useDeviceReadonly = () => useSelector((state) => {
-    const admin = state.session.user?.administrator;
-    const serverReadonly = state.session.server?.readonly;
-    const userReadonly = state.session.user?.readonly;
-    const serverDeviceReadonly = state.session.server?.deviceReadonly;
-    const userDeviceReadonly = state.session.user?.deviceReadonly;
-    return !admin && (serverReadonly || userReadonly || serverDeviceReadonly || userDeviceReadonly);
+  const admin = state.session.user?.administrator;
+  const serverReadonly = state.session.server?.readonly;
+  const userReadonly = state.session.user?.readonly;
+  const serverDeviceReadonly = state.session.server?.deviceReadonly;
+  const userDeviceReadonly = state.session.user?.deviceReadonly;
+  return !admin && (serverReadonly || userReadonly || serverDeviceReadonly || userDeviceReadonly);
 });
 
 export const useEditable = () => useSelector((state) => {
-    const admin = state.session.user?.administrator;
-    const serverReadonly = state.session.server?.readonly;
-    const userReadonly = state.session.user?.readonly;
-    return admin || (!serverReadonly && !userReadonly);
+  const admin = state.session.user?.administrator;
+  const serverReadonly = state.session.server?.readonly;
+  const userReadonly = state.session.user?.readonly;
+  return admin || (!serverReadonly && !userReadonly);
 });
