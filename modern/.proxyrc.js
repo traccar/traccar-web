214e92536c52b8c7b733495a63e34ff0bc81a834
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-06-01 21:57:25 +0430

    Update modern/.proxyrc.js
    Co-authored-by: Anton Tananaev <anton.tananaev@gmail.com>

--- modern/.proxyrc.js
+++ modern/.proxyrc.js
@@ -2,6 +2,6 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   const reactAppURLName = process.env.REACT_APP_URL_NAME ? process.env.REACT_APP_URL_NAME : "localhost"
-  app.use(createProxyMiddleware('/api/socket', { target: `ws://${reactAppURLName }`, ws: true }));
+  app.use(createProxyMiddleware('/api/socket', { target: `ws://${reactAppURLName}`, ws: true }));
   app.use(createProxyMiddleware('/api', { target: `http://${reactAppURLName}` }));
 };
