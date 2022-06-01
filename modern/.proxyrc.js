02713f30c28db4773d10bb8ff4f2a70fe8832e1a
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-06-01 12:06:18 +0430

    Fixing the empty line.

--- modern/.proxyrc.js
+++ modern/.proxyrc.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  const reactAppURLName = process.env.REACT_APP_URL_NAME ? "" : "localhost"
+  const reactAppURLName = process.env.REACT_APP_URL_NAME ? process.env.REACT_APP_URL_NAME : "localhost"
   app.use(createProxyMiddleware('/api/socket', { target: `ws://${reactAppURLName }`, ws: true }));
   app.use(createProxyMiddleware('/api', { target: `http://${reactAppURLName}` }));
 };
