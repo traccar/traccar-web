672d7828afefa07faca7954dfd55b683ae7a68fc
Anton Tananaev <anton@traccar.org>
2022-06-12 07:55:40 -0700

    Add TomTom map

--- modern/src/common/attributes/useCommonUserAttributes.js
+++ modern/src/common/attributes/useCommonUserAttributes.js
@@ -21,6 +21,10 @@ export default (t) => useMemo(() => ({
     name: t('mapOpenWeatherKey'),
     type: 'string',
   },
+  tomTomKey: {
+    name: t('mapTomTomKey'),
+    type: 'string',
+  },
   notificationTokens: {
     name: t('attributeNotificationTokens'),
     type: 'string',
