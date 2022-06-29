520860fbeef74b4527256c5f9641258bdd53e18f
Anton Tananaev <anton@traccar.org>
2022-06-29 09:53:27 -0700

    Update scripts

--- tools/package.sh
+++ tools/package.sh
@@ -4,13 +4,13 @@ cd $(dirname $0)
 
 cd ..
 
-rm -r web/lib
+rm -rf web/lib
 npm install --unsafe-perm
 npm run build
 
 cd modern
 
-rm -r ../web/modern
+rm -rf ../web/modern
 npm install
 npm run build_release
 mv build ../web/modern
