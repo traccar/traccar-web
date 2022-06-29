5eb515b35eb3b7328f15985620b3f919bef5e1cf
Anton Tananaev <anton@traccar.org>
2022-06-29 10:39:43 -0700

    Check in lock files

--- tools/package.sh
+++ tools/package.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 cd $(dirname $0)
-
 cd ..
 
 rm -rf web/lib
-npm install --unsafe-perm
+npm ci --unsafe-perm
 npm run build
 
 cd modern
 
 rm -rf ../web/modern
-npm install
+npm ci
 npm run build_release
 mv build ../web/modern
