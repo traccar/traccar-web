980956695258be1ff464923c76c1c6db1df397c1
Anton Tananaev <anton@traccar.org>
2022-08-06 21:25:33 -0700

    Fix report exporting issue

--- modern/src/service-worker.js
+++ modern/src/service-worker.js
@@ -28,19 +28,28 @@ const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
+
     // If this isn't a navigation, skip.
     if (request.mode !== 'navigate') {
       return false;
-    } // If this is a URL that starts with /_, skip.
+    }
 
+    // If this is a URL that starts with /api/, skip.
+    if (url.pathname.startsWith('/api/')) {
+      return false;
+    }
+
+    // If this is a URL that starts with /_, skip.
     if (url.pathname.startsWith('/_')) {
       return false;
-    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
+    }
+    
+    // If this looks like a URL for a resource, because it contains // a file extension, skip.
     if (url.pathname.match(fileExtensionRegexp)) {
       return false;
-    } // Return true to signal that we want to use the handler.
-
+    }
+    
+    // Return true to signal that we want to use the handler.
     return true;
   },
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
