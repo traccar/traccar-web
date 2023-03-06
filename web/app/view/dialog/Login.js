714576f78b73ba434f2c398dfbdca2bc64081a5b
sajj01 <77871855+sajj01@users.noreply.github.com>
2023-03-07 03:32:12 +0500

    Customisation Done

--- web/app/view/dialog/Login.js
+++ web/app/view/dialog/Login.js
@@ -41,7 +41,7 @@ Ext.define('Traccar.view.dialog.Login', {
 
         items: [{
             xtype: 'image',
-            src: 'logo.svg',
+            src: 'logo.png',
             alt: Strings.loginLogo,
             width: 240,
             height: 64,
