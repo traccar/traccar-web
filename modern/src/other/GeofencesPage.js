fc788646bc720d3cceb639db83094d8540025d3b
Anton Tananaev <anton@traccar.org>
2023-04-18 12:03:30 -0700

    Fix geofence removal (fix #1103)

--- modern/src/other/GeofencesPage.js
+++ modern/src/other/GeofencesPage.js
@@ -32,9 +32,11 @@ const useStyles = makeStyles((theme) => ({
       flexDirection: 'column-reverse',
     },
   },
+  drawer: {
+    zIndex: 1,
+  },
   drawerPaper: {
     position: 'relative',
-    zIndex: 0,
     [theme.breakpoints.up('sm')]: {
       width: theme.dimensions.drawerWidthTablet,
     },
@@ -102,6 +104,7 @@ const GeofencesPage = () => {
     <div className={classes.root}>
       <div className={classes.content}>
         <Drawer
+          className={classes.drawer}
           anchor={isPhone ? 'bottom' : 'left'}
           variant="permanent"
           classes={{ paper: classes.drawerPaper }}
