172646bb707d26780a97838b5d44a2be8eeafbb3
K-J-Dod24 <kieran.dodson@complexus.uk>
2023-05-10 11:47:09 +0100

    removed duplicates

--- modern/src/common/attributes/usePositionAttributes.js
+++ modern/src/common/attributes/usePositionAttributes.js
@@ -196,16 +196,6 @@ export default (t) => useMemo(() => ({
     name: `${t('positionOutput')} 4`,
     type: 'boolean',
   },
-  coolantTemp: {
-    name: t('positionCoolantTemp'),
-    type: 'number',
-    dataType: 'celsius',
-  },
-  engineTemp: {
-    name: t('positionEngineTemp'),
-    type: 'number',
-    dataType: 'celsius',
-  },
   power: {
     name: t('positionPower'),
     type: 'number',
