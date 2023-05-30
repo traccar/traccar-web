5bb0ec8bab20ca3aa2ee50328e81d25c0e986520
Anton Tananaev <anton@traccar.org>
2023-05-30 13:37:55 -0700

    Update JavaScript dependencies

--- modern/src/index.js
+++ modern/src/index.js
@@ -1,6 +1,7 @@
-import '@fontsource/roboto/latin-400.css';
-import '@fontsource/roboto/latin-500.css';
-import '@fontsource/roboto/latin-700.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
