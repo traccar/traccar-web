2e8fe02238412d2a6abdd89b2fc07c86916cdd1d
jjreset <61910024+jjreset@users.noreply.github.com>
2023-05-30 14:57:18 +0900

    add 700

--- modern/src/index.js
+++ modern/src/index.js
@@ -1,5 +1,6 @@
 import '@fontsource/roboto/latin-400.css';
 import '@fontsource/roboto/latin-500.css';
+import '@fontsource/roboto/latin-700.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
