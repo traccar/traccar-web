5f0fb3d0346acca3796b129e304251d8060bff42
Anton Tananaev <anton@traccar.org>
2023-07-03 10:45:42 -0700

    Validate theme colors

--- modern/src/common/theme/palette.js
+++ modern/src/common/theme/palette.js
@@ -2,12 +2,14 @@ import {
   amber, grey, green, indigo, red, common,
 } from '@mui/material/colors';
 
+const validatedColor = (color) => (/^#([0-9A-Fa-f]{3}){1,2}$/.test(color) ? color : null);
+
 export default (server) => {
   const colors = {
     white: common.white,
     background: grey[50],
-    primary: server?.attributes?.colorPrimary || indigo[900],
-    secondary: server?.attributes?.colorSecondary || green[800],
+    primary: validatedColor(server?.attributes?.colorPrimary) || indigo[900],
+    secondary: validatedColor(server?.attributes?.colorSecondary) || green[800],
     positive: green[500],
     medium: amber[700],
     negative: red[500],
