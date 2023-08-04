6d0058534cf5a011165aa09ddddf785a8bb2925e
radioActive DROID <me@godwin.dev>
2023-08-04 13:41:01 +0100

    update server proxy and fix build error

--- modern/vite.config.ts
+++ modern/vite.config.ts
@@ -1,19 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv, transformWithEsbuild } from 'vite'
 import reactRefresh from '@vitejs/plugin-react'
+import envCompatible from 'vite-plugin-env-compatible'
 import svgrPlugin from 'vite-plugin-svgr'
 
-export default defineConfig({
-    build: {
-        outDir: 'build',
-    },
-    plugins: [
-        react(),
-        reactRefresh(),
-        svgrPlugin({
-            svgrOptions: {
-                icon: true
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    const config: any = {
+        plugins: [
+            envCompatible(),
+            reactRefresh(),
+            svgrPlugin({
+                svgrOptions: {
+                    icon: true
+                },
+            }),
+            {
+                name: 'treat-js-files-as-jsx',
+                async transform(code, id) {
+                    if (!id.match(/src\/.*\.js$/)) return null
+                    // Use the exposed transform from vite, instead of directly
+                    return transformWithEsbuild(code, id, {
+                        loader: 'jsx',
+                        jsx: 'automatic',
+                    })
+                },
             },
-        }),
-    ],
-})
+        ],
+        optimizeDeps: {
+            force: true,
+            esbuildOptions: {
+                loader: {
+                    '.js': 'jsx',
+                },
+            },
+        },
+        server: {
+            proxy: {
+                "/api": {
+                    target: `http://${process.env.VITE_URL_NAME}`,
+                    changeOrigin: true,
+                    secure: false,
+                },
+                "/api/socket": {
+                    target: `ws://${process.env.VITE_URL_NAME}`,
+                    changeOrigin: true,
+                    secure: false,
+                    ws: true,
+                },
+            }
+        }
+    };
+
+    return defineConfig(config);
+}
