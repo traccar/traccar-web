8a4365fcc9f19c94f3b17429965112e11cde6da3
Jamie Guthrie <jamie.guthrie@gmail.com>
2023-08-17 13:52:22 +0200

    Syntax fix

--- modern/src/common/components/DelayedTooltip.js
+++ modern/src/common/components/DelayedTooltip.js
@@ -3,14 +3,12 @@ import Tooltip from '@mui/material/Tooltip';
 
 const DelayedTooltip = ({
   ...props
-}) => {
-  (
-    <Tooltip
-      {...props}
-      enterDelay={700}
-      enterNextDelay={700}
-    />
-  );
-};
+}) => (
+  <Tooltip
+    {...props}
+    enterDelay={700}
+    enterNextDelay={700}
+  />
+);
 
 export default DelayedTooltip;
