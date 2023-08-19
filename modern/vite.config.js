296db114132a395b0743732f04bd6ddf6b4edf0f
Anton Tananaev <anton@traccar.org>
2023-08-19 14:02:52 -0700

    Fix lint issues

--- modern/vite.config.js
+++ modern/vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig(() => {
-  return {
-    server: {
-      port: 3000,
-      proxy: {
-        '/api/socket': 'ws://localhost:8082',
-        '/api': 'http://localhost:8082',
-      },
+export default defineConfig(() => ({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api/socket': 'ws://localhost:8082',
+      '/api': 'http://localhost:8082',
     },
-    build: {
-      outDir: 'build',
-    },
-    plugins: [svgr(), react(), VitePWA()],
-  };
-});
+  },
+  build: {
+    outDir: 'build',
+  },
+  plugins: [svgr(), react(), VitePWA()],
+}));
