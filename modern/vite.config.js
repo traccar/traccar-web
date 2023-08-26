5c847625b8bce597bbe05c0acf6ebd3e54eec3e7
Anton Tananaev <anton@traccar.org>
2023-08-26 15:31:10 -0700

    Fix PWA issues

--- modern/vite.config.js
+++ modern/vite.config.js
@@ -14,5 +14,37 @@ export default defineConfig(() => ({
   build: {
     outDir: 'build',
   },
-  plugins: [svgr(), react(), VitePWA()],
+  plugins: [
+    svgr(),
+    react(),
+    VitePWA({
+      workbox: {
+        navigateFallbackDenylist: [/^\/api/],
+      },
+      manifest: {
+        short_name: '${title}',
+        name: '${description}',
+        theme_color: '${colorPrimary}',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '48x48 32x32 16x16',
+            type: 'image/x-icon',
+          },
+          {
+            src: 'logo192.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'logo512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 }));
