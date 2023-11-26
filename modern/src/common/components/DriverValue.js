d90ec814f5b76ec9b733ac77b3c0ea1eb5e7570e
Anton Tananaev <anton@traccar.org>
2023-11-26 07:05:39 -0800

    Fix missing driver issue (fix #1197)

--- modern/src/common/components/DriverValue.js
+++ modern/src/common/components/DriverValue.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 const DriverValue = ({ driverUniqueId }) => {
   const driver = useSelector((state) => state.drivers.items[driverUniqueId]);
 
-  return driver.name;
+  return driver?.name || driverUniqueId;
 };
 
 export default DriverValue;
