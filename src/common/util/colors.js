11dffdeba76e8d906385eb1f6af5ceeda2b90edf
jcardus <asklocation.net@gmail.com>
2024-06-16 23:16:57 +0100

    simplify interpolateColor

--- src/common/util/colors.js
+++ src/common/util/colors.js
@@ -1,17 +1,17 @@
-import { hexToRgb, rgbToHex, decomposeColor } from '@mui/material';
+import { decomposeColor } from '@mui/material';
 
 export const interpolateColor = (color1, color2, factor) => {
   if (factor > 1) factor = 1;
   if (factor < 0) factor = 0;
 
-  const c1 = decomposeColor(hexToRgb(color1)).values;
-  const c2 = decomposeColor(hexToRgb(color2)).values;
+  const c1 = decomposeColor(color1).values;
+  const c2 = decomposeColor(color2).values;
 
   const r = Math.round(c1[0] + factor * (c2[0] - c1[0]));
   const g = Math.round(c1[1] + factor * (c2[1] - c1[1]));
   const b = Math.round(c1[2] + factor * (c2[2] - c1[2]));
 
-  return rgbToHex(`rgb(${r}, ${g}, ${b})`);
+  return `rgb(${r}, ${g}, ${b})`;
 };
 
 export const getSpeedColor = (speed, max) => interpolateColor('#FFFF00', '#FF0000', speed / max);
