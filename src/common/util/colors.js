d0b0878eca47aab1a253d9ed76444de1447960b3
jcardus <joaquim_cardeira@hotmail.com>
2024-06-16 22:38:50 +0100

    simplify interpolateColor

--- src/common/util/colors.js
+++ src/common/util/colors.js
@@ -4,10 +4,8 @@ export const interpolateColor = (color1, color2, factor) => {
   if (factor > 1) factor = 1;
   if (factor < 0) factor = 0;
 
-  const parseRgb = (rgb) => rgb.split(',').map(Number);
-
-  const c1 = parseRgb(hexToRgb(color1));
-  const c2 = parseRgb(hexToRgb(color2));
+  const c1 = hexToRgb(color1).split(',').map(Number);
+  const c2 = hexToRgb(color2).split(',').map(Number);
 
   const r = Math.round(c1[0] + factor * (c2[0] - c1[0]));
   const g = Math.round(c1[1] + factor * (c2[1] - c1[1]));
