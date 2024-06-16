9c946ca3513157b430d5871297180d680024b7ac
jcardus <asklocation.net@gmail.com>
2024-06-16 23:09:40 +0100

    decomposeColor

--- src/common/util/colors.js
+++ src/common/util/colors.js
@@ -1,11 +1,11 @@
-import { hexToRgb, rgbToHex } from '@mui/material';
+import { hexToRgb, rgbToHex, decomposeColor } from '@mui/material';
 
 export const interpolateColor = (color1, color2, factor) => {
   if (factor > 1) factor = 1;
   if (factor < 0) factor = 0;
 
-  const c1 = hexToRgb(color1).split(',').map(Number);
-  const c2 = hexToRgb(color2).split(',').map(Number);
+  const c1 = decomposeColor(hexToRgb(color1)).values;
+  const c2 = decomposeColor(hexToRgb(color2)).values;
 
   const r = Math.round(c1[0] + factor * (c2[0] - c1[0]));
   const g = Math.round(c1[1] + factor * (c2[1] - c1[1]));
