17ade9707e14bd04e071a59a610cd34c4e3780a2
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-06-28 21:36:31 -0500

    Create reportsPaths

--- src/reports/components/ReportsLayout.jsx
+++ src/reports/components/ReportsLayout.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import PageLayout from '../../common/components/PageLayout';
 import ReportsMenu from './ReportsMenu';
 
-const ReportsLayout = () => (
-  <PageLayout
-    menu={<ReportsMenu />}
-    breadcrumbs={['reportTitle', 'reportCombined']}
-  >
-    <Outlet />
-  </PageLayout>
-);
+const reportsPaths = {
+  '/reports/combined': 'reportCombined',
+  '/reports/route': 'reportRoute',
+  '/reports/event': 'reportEvents',
+  '/reports/trip': 'reportTrips',
+  '/reports/stop': 'reportStops',
+  '/reports/summary': 'reportSummary',
+  '/reports/chart': 'reportChart',
+  '/reports/logs': 'statisticsTitle',
+  '/reports/scheduled': 'reportScheduled',
+  '/reports/statistics': 'statisticsTitle',
+};
+
+const ReportsLayout = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <PageLayout
+      menu={<ReportsMenu />}
+      breadcrumbs={['reportTitle', reportsPaths[pathname] || '']}
+    >
+      <Outlet />
+    </PageLayout>
+  );
+};
 
 export default ReportsLayout;
