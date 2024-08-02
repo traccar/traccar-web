8baa61ce7f6972639725775e73abf2c42441b09a
ammaralgorepublic <ammar.mohyuddin@algorepublic.com>
2024-08-02 19:09:05 +0500

    first commit

--- src/common/components/PageLayout.jsx
+++ src/common/components/PageLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   AppBar,
   Breadcrumbs,
@@ -9,22 +9,22 @@ import {
   Typography,
   useMediaQuery,
   useTheme,
-} from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import MenuIcon from '@mui/icons-material/Menu';
-import { useNavigate } from 'react-router-dom';
-import { useTranslation } from './LocalizationProvider';
+} from "@mui/material";
+import makeStyles from "@mui/styles/makeStyles";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "./LocalizationProvider";
 
 const useStyles = makeStyles((theme) => ({
   desktopRoot: {
-    height: '100%',
-    display: 'flex',
+    height: "100%",
+    display: "flex",
   },
   mobileRoot: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
   desktopDrawer: {
     width: theme.dimensions.drawerWidthDesktop,
@@ -37,10 +37,10 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     flexGrow: 1,
-    alignItems: 'stretch',
-    display: 'flex',
-    flexDirection: 'column',
-    overflowY: 'auto',
+    alignItems: "stretch",
+    display: "flex",
+    flexDirection: "column",
+    overflowY: "auto",
   },
 }));
 
@@ -48,19 +48,25 @@ const PageTitle = ({ breadcrumbs }) => {
   const theme = useTheme();
   const t = useTranslation();
 
-  const desktop = useMediaQuery(theme.breakpoints.up('md'));
+  const desktop = useMediaQuery(theme.breakpoints.up("md"));
 
   if (desktop) {
     return (
-      <Typography variant="h6" noWrap>{t(breadcrumbs[0])}</Typography>
+      <Typography variant="h6" noWrap>
+        {t(breadcrumbs[0])}
+      </Typography>
     );
   }
   return (
     <Breadcrumbs>
       {breadcrumbs.slice(0, -1).map((breadcrumb) => (
-        <Typography variant="h6" color="inherit" key={breadcrumb}>{t(breadcrumb)}</Typography>
+        <Typography variant="h6" color="inherit" key={breadcrumb}>
+          {t(breadcrumb)}
+        </Typography>
       ))}
-      <Typography variant="h6" color="textPrimary">{t(breadcrumbs[breadcrumbs.length - 1])}</Typography>
+      <Typography variant="h6" color="textPrimary">
+        {t(breadcrumbs[breadcrumbs.length - 1])}
+      </Typography>
     </Breadcrumbs>
   );
 };
@@ -70,7 +76,7 @@ const PageLayout = ({ menu, breadcrumbs, children }) => {
   const theme = useTheme();
   const navigate = useNavigate();
 
-  const desktop = useMediaQuery(theme.breakpoints.up('md'));
+  const desktop = useMediaQuery(theme.breakpoints.up("md"));
 
   const [openDrawer, setOpenDrawer] = useState(false);
 
@@ -82,7 +88,12 @@ const PageLayout = ({ menu, breadcrumbs, children }) => {
         classes={{ paper: classes.desktopDrawer }}
       >
         <Toolbar>
-          <IconButton color="inherit" edge="start" sx={{ mr: 2 }} onClick={() => navigate('/')}>
+          <IconButton
+            color="inherit"
+            edge="start"
+            sx={{ mr: 2 }}
+            onClick={() => navigate("/")}
+          >
             <ArrowBackIcon />
           </IconButton>
           <PageTitle breadcrumbs={breadcrumbs} />
@@ -102,9 +113,18 @@ const PageLayout = ({ menu, breadcrumbs, children }) => {
       >
         {menu}
       </Drawer>
-      <AppBar className={classes.mobileToolbar} position="static" color="inherit">
+      <AppBar
+        className={classes.mobileToolbar}
+        position="static"
+        color="inherit"
+      >
         <Toolbar>
-          <IconButton color="inherit" edge="start" sx={{ mr: 2 }} onClick={() => setOpenDrawer(true)}>
+          <IconButton
+            color="inherit"
+            edge="start"
+            sx={{ mr: 2 }}
+            onClick={() => setOpenDrawer(true)}
+          >
             <MenuIcon />
           </IconButton>
           <PageTitle breadcrumbs={breadcrumbs} />
--- src/reports/components/ReportsMenu.jsx
+++ src/reports/components/ReportsMenu.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 import {
-  Divider, List, ListItemButton, ListItemIcon, ListItemText,
-} from '@mui/material';
-import StarIcon from '@mui/icons-material/Star';
-import TimelineIcon from '@mui/icons-material/Timeline';
-import PauseCircleFilledIcon from '@mui/icons-material/PauseCircleFilled';
-import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import RouteIcon from '@mui/icons-material/Route';
-import EventRepeatIcon from '@mui/icons-material/EventRepeat';
-import NotesIcon from '@mui/icons-material/Notes';
-import { Link, useLocation } from 'react-router-dom';
-import { useTranslation } from '../../common/components/LocalizationProvider';
-import { useAdministrator, useRestriction } from '../../common/util/permissions';
+  Divider,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
+import TimelineIcon from "@mui/icons-material/Timeline";
+import PauseCircleFilledIcon from "@mui/icons-material/PauseCircleFilled";
+import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import RouteIcon from "@mui/icons-material/Route";
+import EventRepeatIcon from "@mui/icons-material/EventRepeat";
+import NotesIcon from "@mui/icons-material/Notes";
+import { Link, useLocation } from "react-router-dom";
+import { useTranslation } from "../../common/components/LocalizationProvider";
+import {
+  useAdministrator,
+  useRestriction,
+} from "../../common/util/permissions";
 
-const MenuItem = ({
-  title, link, icon, selected,
-}) => (
+const MenuItem = ({ title, link, icon, selected }) => (
   <ListItemButton key={link} component={Link} to={link} selected={selected}>
     <ListItemIcon>{icon}</ListItemIcon>
     <ListItemText primary={title} />
@@ -29,57 +34,57 @@ const MenuItem = ({
 const ReportsMenu = () => {
   const t = useTranslation();
   const location = useLocation();
-
+  console.log(t("reportCombined"));
   const admin = useAdministrator();
-  const readonly = useRestriction('readonly');
+  const readonly = useRestriction("readonly");
 
   return (
     <>
       <List>
         <MenuItem
-          title={t('reportCombined')}
+          title={t("reportCombined")}
           link="/reports/combined"
           icon={<StarIcon />}
-          selected={location.pathname === '/reports/combined'}
+          selected={location.pathname === "/reports/combined"}
         />
         <MenuItem
-          title={t('reportRoute')}
+          title={t("reportRoute")}
           link="/reports/route"
           icon={<TimelineIcon />}
-          selected={location.pathname === '/reports/route'}
+          selected={location.pathname === "/reports/route"}
         />
         <MenuItem
-          title={t('reportEvents')}
+          title={t("reportEvents")}
           link="/reports/event"
           icon={<NotificationsActiveIcon />}
-          selected={location.pathname === '/reports/event'}
+          selected={location.pathname === "/reports/event"}
         />
         <MenuItem
-          title={t('reportTrips')}
+          title={t("reportTrips")}
           link="/reports/trip"
           icon={<PlayCircleFilledIcon />}
-          selected={location.pathname === '/reports/trip'}
+          selected={location.pathname === "/reports/trip"}
         />
         <MenuItem
-          title={t('reportStops')}
+          title={t("reportStops")}
           link="/reports/stop"
           icon={<PauseCircleFilledIcon />}
-          selected={location.pathname === '/reports/stop'}
+          selected={location.pathname === "/reports/stop"}
         />
         <MenuItem
-          title={t('reportSummary')}
+          title={t("reportSummary")}
           link="/reports/summary"
           icon={<FormatListBulletedIcon />}
-          selected={location.pathname === '/reports/summary'}
+          selected={location.pathname === "/reports/summary"}
         />
         <MenuItem
-          title={t('reportChart')}
+          title={t("reportChart")}
           link="/reports/chart"
           icon={<TrendingUpIcon />}
-          selected={location.pathname === '/reports/chart'}
+          selected={location.pathname === "/reports/chart"}
         />
         <MenuItem
-          title={t('reportReplay')}
+          title={t("reportReplay")}
           link="/replay"
           icon={<RouteIcon />}
         />
@@ -87,25 +92,25 @@ const ReportsMenu = () => {
       <Divider />
       <List>
         <MenuItem
-          title={t('sharedLogs')}
+          title={t("sharedLogs")}
           link="/reports/logs"
           icon={<NotesIcon />}
-          selected={location.pathname === '/reports/logs'}
+          selected={location.pathname === "/reports/logs"}
         />
         {!readonly && (
           <MenuItem
-            title={t('reportScheduled')}
+            title={t("reportScheduled")}
             link="/reports/scheduled"
             icon={<EventRepeatIcon />}
-            selected={location.pathname === '/reports/scheduled'}
+            selected={location.pathname === "/reports/scheduled"}
           />
         )}
         {admin && (
           <MenuItem
-            title={t('statisticsTitle')}
+            title={t("statisticsTitle")}
             link="/reports/statistics"
             icon={<BarChartIcon />}
-            selected={location.pathname === '/reports/statistics'}
+            selected={location.pathname === "/reports/statistics"}
           />
         )}
       </List>
