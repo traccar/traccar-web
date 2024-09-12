e83c1391ee808e98bb882ad067fb8d60eb2b2da2
Mauricio Herrera <wmhr28@hotmail.com>
2024-09-12 09:50:40 -0500

    personalizaciones

--- vite.config.js
+++ vite.config.js
@@ -26,9 +26,9 @@ export default defineConfig(() => ({
         globPatterns: ['**/*.{js,css,html,woff,woff2,mp3}'],
       },
       manifest: {
-        short_name: '${title}',
-        name: '${description}',
-        theme_color: '${colorPrimary}',
+        short_name: 'Mi Satelite GPS',
+        name: 'Mi Satelite - GPS Tracking Solution',
+        theme_color: '#1a237e',
         icons: [
           {
             src: 'pwa-64x64.png',
