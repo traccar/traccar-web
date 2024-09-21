2154ff10c729d6f1320798aaf0c2f78d603481f6
Anton Tananaev <anton@traccar.org>
2024-09-21 13:37:10 -0700

    Inactivity attributes for groups

--- src/common/attributes/useDeviceAttributes.js
+++ src/common/attributes/useDeviceAttributes.js
@@ -22,12 +22,4 @@ export default (t) => useMemo(() => ({
     name: t('sharedTimezone'),
     type: 'string',
   },
-  deviceInactivityStart: {
-    name: t('attributeDeviceInactivityStart'),
-    type: 'number',
-  },
-  deviceInactivityPeriod: {
-    name: t('attributeDeviceInactivityPeriod'),
-    type: 'number',
-  },
 }), [t]);
