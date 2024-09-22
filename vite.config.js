bd194fe67e390581aa918c0daa6f466d63e61a0c
aspriest <adenpriest@icloud.com>
2024-09-22 16:11:05 +0930

    updated fleet branding and icons

--- vite.config.js
+++ vite.config.js
@@ -31,17 +31,17 @@ export default defineConfig(() => ({
         theme_color: '${colorPrimary}',
         icons: [
           {
-            src: 'pwa-64x64.png',
+            src: 'logo192.png',
             sizes: '64x64',
             type: 'image/png',
           },
           {
-            src: 'pwa-192x192.png',
+            src: 'logo192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'logo512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
