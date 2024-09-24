67111d18ebaf6a5321df208d997f9056f3114a8f
Hasan A <hasan@algorepublic.com>
2024-09-24 22:40:12 +0500

    ui updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,98 @@
-import React from "react";
-import { Outlet, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { LinearProgress, useMediaQuery, useTheme } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
-import BottomMenu from "./common/components/BottomMenu";
-import SocketController from "./SocketController";
-import CachingController from "./CachingController";
-import { useCatch, useEffectAsync } from "./reactHelper";
-import { sessionActions } from "./store";
-import UpdateController from "./UpdateController";
-import TermsDialog from "./common/components/TermsDialog";
+import React from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useMediaQuery, useTheme } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
+import BottomMenu from './common/components/BottomMenu';
+import SocketController from './SocketController';
+import CachingController from './CachingController';
+import { useCatch, useEffectAsync } from './reactHelper';
+import { sessionActions } from './store';
+import UpdateController from './UpdateController';
+import TermsDialog from './common/components/TermsDialog';
+import Loader from './common/components/Loader';
 
 const useStyles = makeStyles(() => ({
-  page: {
-    flexGrow: 1,
-    overflow: "auto",
-  },
-  menu: {
-    zIndex: 4,
-  },
+	page: {
+		flexGrow: 1,
+		overflow: 'auto',
+	},
+	menu: {
+		zIndex: 4,
+	},
 }));
 
 const App = () => {
-  const classes = useStyles();
-  const theme = useTheme();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const classes = useStyles();
+	const theme = useTheme();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  const desktop = useMediaQuery(theme.breakpoints.up("md"));
+	const desktop = useMediaQuery(theme.breakpoints.up('md'));
 
-  const newServer = useSelector((state) => state.session.server.newServer);
-  const termsUrl = useSelector(
-    (state) => state.session.server.attributes.termsUrl
-  );
-  const user = useSelector((state) => state.session.user);
+	const newServer = useSelector(state => state.session.server.newServer);
+	const termsUrl = useSelector(
+		state => state.session.server.attributes.termsUrl
+	);
+	const user = useSelector(state => state.session.user);
 
-  const acceptTerms = useCatch(async () => {
-    const response = await fetch(`/api/users/${user.id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        ...user,
-        attributes: { ...user.attributes, termsAccepted: true },
-      }),
-    });
-    if (response.ok) {
-      dispatch(sessionActions.updateUser(await response.json()));
-    } else {
-      throw Error(await response.text());
-    }
-  });
+	const acceptTerms = useCatch(async () => {
+		const response = await fetch(`/api/users/${user.id}`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				...user,
+				attributes: { ...user.attributes, termsAccepted: true },
+			}),
+		});
+		if (response.ok) {
+			dispatch(sessionActions.updateUser(await response.json()));
+		} else {
+			throw Error(await response.text());
+		}
+	});
 
-  useEffectAsync(async () => {
-    if (!user) {
-      const response = await fetch("/api/session");
-      if (response.ok) {
-        dispatch(sessionActions.updateUser(await response.json()));
-      } else if (newServer) {
-        navigate("/register");
-      } else {
-        navigate("/login");
-      }
-    }
-    return null;
-  }, [user]);
+	useEffectAsync(async () => {
+		if (!user) {
+			const response = await fetch('/api/session');
+			if (response.ok) {
+				dispatch(sessionActions.updateUser(await response.json()));
+			} else if (newServer) {
+				navigate('/register');
+			} else {
+				navigate('/login');
+			}
+		}
+		return null;
+	}, [user]);
 
-  if (user == null) {
-
-    return (<Loader />);
-  }
-  if (termsUrl && !user.attributes.termsAccepted) {
-    return (
-      <TermsDialog
-        open
-        onCancel={() => navigate("/login")}
-        onAccept={() => acceptTerms()}
-      />
-    );
-  }
-  return (
-    <>
-      <SocketController />
-      <CachingController />
-      <UpdateController />
-      <div className={classes.page}>
-        <Outlet />
-      </div>
-      {!desktop && (
-        <div className={classes.menu}>
-          <BottomMenu />
-        </div>
-      )}
-    </>
-  );
+	if (user == null) {
+		return <Loader />;
+	}
+	if (termsUrl && !user.attributes.termsAccepted) {
+		return (
+			<TermsDialog
+				open
+				onCancel={() => navigate('/login')}
+				onAccept={() => acceptTerms()}
+			/>
+		);
+	}
+	return (
+		<>
+			<SocketController />
+			<CachingController />
+			<UpdateController />
+			<div className={classes.page}>
+				<Outlet />
+			</div>
+			{!desktop && (
+				<div className={classes.menu}>
+					<BottomMenu />
+				</div>
+			)}
+		</>
+	);
 };
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -5,49 +5,49 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 /* eslint-disable no-template-curly-in-string */
 export default defineConfig(() => ({
-  server: {
-    port: 3000,
-    proxy: {
-      '/api/socket': 'ws://localhost:8082',
-      '/api': 'http://localhost:8082',
-    },
-  },
-  build: {
-    outDir: 'build',
-  },
-  plugins: [
-    svgr(),
-    react(),
-    VitePWA({
-      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png'],
-      workbox: {
-        navigateFallbackDenylist: [/^\/api/],
-        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
-        globPatterns: ['**/*.{js,css,html,woff,woff2,mp3}'],
-      },
-      manifest: {
-        short_name: '${title}',
-        name: '${description}',
-        theme_color: '${colorPrimary}',
-        icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-  ],
+	server: {
+		port: 3000,
+		proxy: {
+			'/api/socket': 'ws://tracker.algorepublic.com:8082',
+			'/api': 'http://tracker.algorepublic.com:8082',
+		},
+	},
+	build: {
+		outDir: 'build',
+	},
+	plugins: [
+		svgr(),
+		react(),
+		VitePWA({
+			includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png'],
+			workbox: {
+				navigateFallbackDenylist: [/^\/api/],
+				maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+				globPatterns: ['**/*.{js,css,html,woff,woff2,mp3}'],
+			},
+			manifest: {
+				short_name: '${title}',
+				name: '${description}',
+				theme_color: '${colorPrimary}',
+				icons: [
+					{
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+		}),
+	],
 }));
