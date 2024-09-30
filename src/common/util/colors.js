12c8aac607cece8d3229f07a5ebef00446ff63b4
Kalabint <Kalabint@gmail.com>
2024-09-30 19:33:11 +0000

    Corrected lint issues

--- src/common/util/colors.js
+++ src/common/util/colors.js
@@ -2,30 +2,30 @@
 const turboPolynomials = {
   r: [0.13572138, 4.61539260, -42.66032258, 132.13108234, -152.94239396, 59.28637943],
   g: [0.09140261, 2.19418839, 4.84296658, -14.18503333, 4.27729857, 2.82956604],
-  b: [0.10667330, 12.64194608, -60.58204836, 110.36276771, -89.90310912, 27.34824973]
+  b: [0.10667330, 12.64194608, -60.58204836, 110.36276771, -89.90310912, 27.34824973],
 };
 
-function interpolateChannel(t, coeffs) {
+function interpolateChannel(normalizedValue, coeffs) {
   let result = 0;
-  let tPower = 1;
-  for (let i = 0; i < coeffs.length; i++) {
-    result += coeffs[i] * tPower;
-    tPower *= t;
+  for (let i = 0; i < coeffs.length; i += 1) {
+    result += coeffs[i] * (normalizedValue ** i);
   }
   return Math.max(0, Math.min(1, result));
 }
 
-function interpolateTurbo(t) {
-  t = Math.max(0, Math.min(1, t));
+function interpolateTurbo(value) {
+  const normalizedValue = Math.max(0, Math.min(1, value));
   return [
-    Math.round(255 * interpolateChannel(t, turboPolynomials.r)),
-    Math.round(255 * interpolateChannel(t, turboPolynomials.g)),
-    Math.round(255 * interpolateChannel(t, turboPolynomials.b))
+    Math.round(255 * interpolateChannel(normalizedValue, turboPolynomials.r)),
+    Math.round(255 * interpolateChannel(normalizedValue, turboPolynomials.g)),
+    Math.round(255 * interpolateChannel(normalizedValue, turboPolynomials.b)),
   ];
 }
 
-export function getSpeedColor(speed, max) {
-  const factor = Math.max(0, Math.min(1, speed / max));
-  const [r, g, b] = interpolateTurbo(factor);
+const getSpeedColor = (speed, maxSpeed) => {
+  const normalizedSpeed = Math.max(0, Math.min(1, speed / maxSpeed));
+  const [r, g, b] = interpolateTurbo(normalizedSpeed);
   return `rgb(${r}, ${g}, ${b})`;
-}
+};
+
+export default getSpeedColor;
