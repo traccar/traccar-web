6d434ef41cccd41d15355a3870b0e0ac19edcebb
riq <prab@ridesiq.com>
2025-01-08 22:57:06 -0500

    IoT Rebrand

--- src/common/theme/palette.js
+++ src/common/theme/palette.js
@@ -8,10 +8,10 @@ export default (server, darkMode) => ({
     default: darkMode ? grey[900] : grey[50],
   },
   primary: {
-    main: validatedColor(server?.attributes?.colorPrimary) || (darkMode ? indigo[200] : indigo[900]),
+    main: validatedColor(server?.attributes?.colorPrimary) || '#678FCA',
   },
   secondary: {
-    main: validatedColor(server?.attributes?.colorSecondary) || (darkMode ? green[200] : green[800]),
+    main: validatedColor(server?.attributes?.colorSecondary) || '#99D5C9',
   },
   neutral: {
     main: grey[500],
