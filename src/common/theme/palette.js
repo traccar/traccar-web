a2b12d3dc717760936f8834730d5a0107cbf8676
ridesiq <prab@ridesiq.com>
2025-01-13 08:55:48 -0500

    Update palette.js
    unused colors removed

--- src/common/theme/palette.js
+++ src/common/theme/palette.js
@@ -1,4 +1,4 @@
-import { grey, green, indigo } from '@mui/material/colors';
+import { grey} from '@mui/material/colors';
 
 const validatedColor = (color) => (/^#([0-9A-Fa-f]{3}){1,2}$/.test(color) ? color : null);
 
