d65d93cc4a87ed6264bee67d47e1f53474f6ecfb
Anton Tananaev <anton@traccar.org>
2025-04-18 16:04:20 -0700

    Hide altitude unit attribute

--- src/settings/components/EditAttributesAccordion.jsx
+++ src/settings/components/EditAttributesAccordion.jsx
@@ -112,7 +112,7 @@ const EditAttributesAccordion = ({ attribute, attributes, setAttributes, definit
   const convertToList = (attributes) => {
     const booleanList = [];
     const otherList = [];
-    const excludeAttributes = ['speedUnit', 'distanceUnit', 'volumeUnit', 'timezone'];
+    const excludeAttributes = ['speedUnit', 'distanceUnit', 'altitudeUnit', 'volumeUnit', 'timezone'];
     Object.keys(attributes || []).filter((key) => !excludeAttributes.includes(key)).forEach((key) => {
       const value = attributes[key];
       const type = getAttributeType(value);
