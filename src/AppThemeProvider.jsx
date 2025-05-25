85c43c72dcad9e896baf3a77738cc900d55b3784
Anton Tananaev <anton@traccar.org>
2025-05-25 14:12:42 -0700

    Remove unnecessary stuff

--- src/AppThemeProvider.jsx
+++ src/AppThemeProvider.jsx
@@ -1,22 +1,7 @@
 import { useSelector } from 'react-redux';
 import { ThemeProvider, useMediaQuery } from '@mui/material';
-import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
-import { prefixer } from 'stylis';
-import rtlPlugin from 'stylis-plugin-rtl';
 import theme from './common/theme';
 import { useLocalization } from './common/components/LocalizationProvider';
-
-const cache = {
-  ltr: createCache({
-    key: 'muiltr',
-    stylisPlugins: [prefixer],
-  }),
-  rtl: createCache({
-    key: 'muirtl',
-    stylisPlugins: [prefixer, rtlPlugin],
-  }),
-};
 
 const AppThemeProvider = ({ children }) => {
   const server = useSelector((state) => state.session.server);
@@ -29,11 +14,9 @@ const AppThemeProvider = ({ children }) => {
   const themeInstance = theme(server, darkMode, direction);
 
   return (
-    <CacheProvider value={cache[direction]}>
-      <ThemeProvider theme={themeInstance}>
-        {children}
-      </ThemeProvider>
-    </CacheProvider>
+    <ThemeProvider theme={themeInstance}>
+      {children}
+    </ThemeProvider>
   );
 };
 
