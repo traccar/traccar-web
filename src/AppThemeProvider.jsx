6fa4807381b2884bbc9b68d99bf5474c20574177
Anton Tananaev <anton@traccar.org>
2025-05-25 15:42:05 -0700

    Revert "Remove unnecessary stuff"
    This reverts commit 85c43c72

--- src/AppThemeProvider.jsx
+++ src/AppThemeProvider.jsx
@@ -1,7 +1,22 @@
 import { useSelector } from 'react-redux';
 import { ThemeProvider, useMediaQuery } from '@mui/material';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { prefixer } from 'stylis';
+import rtlPlugin from 'stylis-plugin-rtl';
 import theme from './common/theme';
 import { useLocalization } from './common/components/LocalizationProvider';
+
+const cache = {
+  ltr: createCache({
+    key: 'muiltr',
+    stylisPlugins: [prefixer],
+  }),
+  rtl: createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+  }),
+};
 
 const AppThemeProvider = ({ children }) => {
   const server = useSelector((state) => state.session.server);
@@ -14,9 +29,11 @@ const AppThemeProvider = ({ children }) => {
   const themeInstance = theme(server, darkMode, direction);
 
   return (
-    <ThemeProvider theme={themeInstance}>
-      {children}
-    </ThemeProvider>
+    <CacheProvider value={cache[direction]}>
+      <ThemeProvider theme={themeInstance}>
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
   );
 };
 
