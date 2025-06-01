c887eea91d2223991853325a27c6f27e37191b4e
Anton Tananaev <anton@traccar.org>
2025-06-01 14:44:53 -0700

    Update throttling middleware

--- src/store/throttleMiddleware.js
+++ src/store/throttleMiddleware.js
@@ -1,35 +1,37 @@
-import { batch } from 'react-redux';
-
-const threshold = 3;
+const threshold = 5;
 const interval = 1500;
+
+const debugMode = process.env.NODE_ENV === 'development';
+const debugLog = (message) => {
+  if (debugMode) console.log(message);
+}
 
 export default () => (next) => {
   const buffer = [];
-  let throttle = false;
+  let throttled = false;
   let counter = 0;
 
   setInterval(() => {
-    if (throttle) {
-      if (buffer.length < threshold) {
-        throttle = false;
-      }
-      batch(() => buffer.splice(0, buffer.length).forEach((action) => next(action)));
-    } else {
-      if (counter > threshold) {
-        throttle = true;
-      }
-      counter = 0;
+    if (throttled) {
+      const flushed = buffer.splice(0, buffer.length);
+      flushed.forEach(next);
+      debugLog(`Throttle flushed ${flushed.length}`);
+      throttled = flushed.length >= threshold;
+      if (!throttled) debugLog(`Throttle stopped`);
     }
+    counter = 0;
   }, interval);
 
   return (action) => {
-    if (action.type === 'devices/update' || action.type === 'positions/update') {
-      if (throttle) {
-        buffer.push(action);
-        return null;
-      }
-      counter += 1;
-      return next(action);
+    if (action.type !== 'devices/update' && action.type !== 'session/updatePositions') return next(action);
+    if (throttled) {
+      buffer.push(action);
+      return;
+    }
+    counter += 1;
+    if (counter >= threshold) {
+      if (!throttled) debugLog(`Throttle started`);
+      throttled = true;
     }
     return next(action);
   };
