b72c8322ab3dacc7f308ee0768f50392f3671b78
Anton Tananaev <anton@traccar.org>
2025-07-01 22:36:49 -0700

    Improve throttling middleware

--- src/store/throttleMiddleware.js
+++ src/store/throttleMiddleware.js
@@ -1,44 +1,82 @@
 import { sessionActions } from './session';
 import { devicesActions } from './devices';
 
-const threshold = 5;
-const interval = 1500;
+const threshold = 3; // per second
+const minInterval = 1500;
+const maxInterval = 30000;
+const scaleFactor = 1000;
 
 // eslint-disable-next-line no-undef
 const debugMode = process.env.NODE_ENV === 'development';
-const debugLog = (message) => {
-  if (debugMode) console.log(message);
-}
+const debugLog = (message) => debugMode && console.log(message);
 
 export default () => (next) => {
   const buffer = [];
   let throttled = false;
   let counter = 0;
+  let currentInterval = minInterval;
 
-  setInterval(() => {
+  const tick = () => {
     if (throttled) {
+      const start = performance.now();
+
       const flushed = buffer.splice(0, buffer.length);
-      flushed.forEach(next);
-      debugLog(`Throttle flushed ${flushed.length}`);
-      throttled = flushed.length >= threshold;
-      if (!throttled) debugLog(`Throttle stopped`);
+
+      const deviceUpdates = {};
+      const positionUpdates = {};
+
+      flushed.forEach((action) => {
+        if (action.type === devicesActions.update.type) {
+          action.payload.forEach(item => deviceUpdates[item.id] = item);
+        } else if (action.type === sessionActions.updatePositions.type) {
+          action.payload.forEach(item => positionUpdates[item.deviceId] = item);
+        }
+      });
+
+      const mergedDeviceUpdates = Object.values(deviceUpdates);
+      if (mergedDeviceUpdates.length) {
+        next({ type: devicesActions.update.type, payload: mergedDeviceUpdates });
+      }
+
+      const mergedPositionUpdates = Object.values(positionUpdates);
+      if (mergedPositionUpdates.length) {
+        next({ type: sessionActions.updatePositions.type, payload: mergedPositionUpdates });
+      }
+
+      const totalTime = performance.now() - start;
+      debugLog(`Flushed ${mergedDeviceUpdates.length + mergedPositionUpdates.length} / ${flushed.length} events in ${totalTime.toFixed(2)} ms`);
+      currentInterval = Math.min(Math.max(totalTime * scaleFactor, minInterval), maxInterval);
+    }
+
+    const shouldThrottle = (counter * 1000 / currentInterval) > threshold;
+    if (throttled != shouldThrottle) {
+      debugLog(`Throttling ${shouldThrottle}`);
+      throttled = shouldThrottle;
     }
     counter = 0;
-  }, interval);
+    if (!throttled) currentInterval = minInterval;
+    setTimeout(tick, currentInterval);
+  };
+
+  setTimeout(tick, currentInterval);
 
   return (action) => {
     if (action.type !== devicesActions.update.type && action.type !== sessionActions.updatePositions.type) {
       return next(action);
     }
+
+    counter += 1;
+
     if (throttled) {
       buffer.push(action);
       return;
     }
-    counter += 1;
-    if (counter >= threshold) {
-      if (!throttled) debugLog(`Throttle started`);
+
+    if ((counter * 1000 / currentInterval) > threshold) {
+      if (!throttled) debugLog(`Throttling started`);
       throttled = true;
     }
+
     return next(action);
   };
 };
