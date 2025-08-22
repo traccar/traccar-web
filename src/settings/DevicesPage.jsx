068928ee7374bfc99a352e9c6f31318d5e2a919c
Anton Tananaev <anton@traccar.org>
2025-08-21 17:35:41 -0700

    Fix devices report

--- src/settings/DevicesPage.jsx
+++ src/settings/DevicesPage.jsx
@@ -63,7 +63,7 @@ const DevicesPage = () => {
       [t('deviceModel')]: item.model,
       [t('deviceContact')]: item.contact,
       [t('userExpirationTime')]: formatTime(item.expirationTime, 'date'),
-      [t('positionAddress')]: positions[item.id].address,
+      [t('positionAddress')]: positions[item.id]?.address || '',
     }));
 
     const workbook = new ExcelJS.Workbook();
@@ -145,7 +145,7 @@ const DevicesPage = () => {
                   <AddressValue
                     latitude={positions[item.id].latitude}
                     longitude={positions[item.id].longitude}
-                    originalAddress={positions[item.id].address}
+                    originalAddress={positions[item.id]?.address}
                   />
                 )}
               </TableCell>
