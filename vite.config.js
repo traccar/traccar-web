beeec62d8fbff2e508b282257ab052ca1d8573ab
Anton Tananaev <anton@traccar.org>
2025-09-01 09:54:34 -0700

    Move to SWC compiler

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import { VitePWA } from 'vite-plugin-pwa';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
