444b1548d852851d9d754513b51c0d974ba7bbd3
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-12-11 22:10:35 +0100

    Fix chart tooltip on darkmode
    Basically reverts https://github.com/traccar/traccar-web/commit/74a1c90074f04945e47e2ad5c4a7aece380b1087

--- src/reports/ChartReportPage.jsx
+++ src/reports/ChartReportPage.jsx
@@ -170,7 +170,7 @@ const ChartReportPage = () => {
               />
               <CartesianGrid stroke={theme.palette.divider} strokeDasharray="3 3" />
               <Tooltip
-                wrapperStyle={{ backgroundColor: theme.palette.background.default, color: theme.palette.text.primary }}
+                contentStyle={{ backgroundColor: theme.palette.background.default, color: theme.palette.text.primary }}
                 formatter={(value, key) => [value, positionAttributes[key]?.name || key]}
                 labelFormatter={(value) => formatTime(value, 'seconds')}
               />
