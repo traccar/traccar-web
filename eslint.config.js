7bd0dd011115b2fab301e04ee7163bcae8bdeefb
Anton Tananaev <anton@traccar.org>
2026-02-09 17:46:42 -0800

    chore(lint): switch to eslint recommended with prettier integration

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,18 @@
 import js from '@eslint/js';
-import airbnb from 'eslint-config-airbnb';
 import react from 'eslint-plugin-react';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
 import importPlugin from 'eslint-plugin-import';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 
 export default [
   {
-    ignores: [
-      'build/**',
-      'switcher.js',
-      'theme.js',
-      'vite.config.js',
-    ],
+    ignores: ['build/**', 'switcher.js', 'theme.js', 'vite.config.js'],
   },
   js.configs.recommended,
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       sourceType: 'module',
       parserOptions: { ecmaFeatures: { jsx: true } },
       globals: {
@@ -28,12 +22,11 @@ export default [
     },
     plugins: {
       react,
-      'jsx-a11y': jsxA11y,
       import: importPlugin,
     },
     settings: {
-      'react': {
-        version: 'detect'
+      react: {
+        version: 'detect',
       },
       'import/resolver': {
         node: {
@@ -42,30 +35,28 @@ export default [
       },
     },
     rules: {
-      ...airbnb.rules,
-      'max-len': 'off',
       'no-shadow': 'off',
-      'no-return-assign': 'off',
-      'no-param-reassign': 'off',
       'no-prototype-builtins': 'off',
-      'object-curly-newline': ['warn', {
-        ObjectExpression: { minProperties: 8, multiline: true, consistent: true },
-        ObjectPattern: { minProperties: 8, multiline: true, consistent: true },
-        ImportDeclaration: { minProperties: 4, multiline: true, consistent: true },
-        ExportDeclaration: { minProperties: 4, multiline: true, consistent: true }
-      }],
-      'import/no-unresolved': ['warn', {
-        ignore: ['\\.svg', 'virtual:']
-      }],
-      'react/function-component-definition': ['warn', {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function'
-      }],
+      'import/no-unresolved': [
+        'warn',
+        {
+          ignore: ['\\.svg', 'virtual:'],
+        },
+      ],
+      'import/named': 'error',
+      'import/default': 'error',
+      'import/namespace': 'error',
+      'react/function-component-definition': [
+        'warn',
+        {
+          namedComponents: 'arrow-function',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
       'react/jsx-props-no-spreading': 'off',
       'react/jsx-uses-vars': 'error',
-      'jsx-a11y/anchor-is-valid': 'off',
-      'jsx-a11y/label-has-associated-control': 'off',
       'react/prop-types': 'off',
     },
   },
+  prettierRecommended,
 ];
